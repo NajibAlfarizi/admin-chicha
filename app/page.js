478bd86d5e004605c23366b58cd29e17dd7ef083
unknown <najibalfarizi5@gmail.com>
2025-04-10 16:57:38 +0700

    refactor: replace Dashboard with AdminLogin component in Home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import Dashboard from "./admin/dashboard/page";
+
+import AdminLogin from "./login/page";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl font-bold">Welcome to Chicha Mobile</h1>
-      <Dashboard />
-    </main>
+    <html lang="en">
+      <body>
+        <AdminLogin />
+      </body>
+    </html>
   );
 }
